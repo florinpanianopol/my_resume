f3b62aa4d62467175d55632995cf383b404b83b5
florin <kirk_metalhead@yahoo.com>
2021-04-15 00:00:52 +0300

    user module additional elements

--- MyResumeWebParent/MyResumeBackEnd/src/main/java/com/myresume/admin/MvcConfigUser.java
+++ MyResumeWebParent/MyResumeBackEnd/src/main/java/com/myresume/admin/MvcConfigUser.java
@@ -11,21 +11,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfigUser implements WebMvcConfigurer {
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		
-		String dirName = "profile-photos";
-		Path userProfilePhotosDir = Paths.get(dirName);
-		
-		String userProfilePhotosPath = userProfilePhotosDir.toFile().getAbsolutePath();
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        String dirName = "user-photos";
+        Path userProfilePhotosDir = Paths.get(dirName);
+
+        String userProfilePhotosPath = userProfilePhotosDir.toFile().getAbsolutePath();
 //		String userProfilePhotosPath = "D:\\backup_proiecte\\my_java_resume\\MyResumeProject\\MyResumeWebParent\\MyResumeBackEnd\\profile-photos";
 //		System.out.print(userProfilePhotosPath);
-		registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+userProfilePhotosPath +
-				"/");
+        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+userProfilePhotosPath +
+                "/");
 //		registry.addResourceHandler("/myresumeadmin/**").addResourceLocations( "localhost:8080/myresumeadmin");
 //		 registry.addResourceHandler("/profile-photos/**").addResourceLocations("classpath:/profile-photos/");
-	} 
+    }
 
 }
